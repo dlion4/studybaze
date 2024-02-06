60ffd4ed680ad354dae7f23be2680b215c1f32af
Jeckonia Kwasa <inctifra@gmail.com>
2024-02-06 11:03:20 +0300

    manage.py update

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
 import os
 import sys
 from pathlib import Path
@@ -25,31 +24,8 @@ if __name__ == "__main__":
         raise
 
     # This allows easy placement of apps within the interior
-    # essayfeeds directory.
+    # {{ cookiecutter.project_slug }} directory.
     current_path = Path(__file__).parent.resolve()
     sys.path.append(str(current_path / "essayfeeds"))
 
     execute_from_command_line(sys.argv)
-=======
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
-
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freelance.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
->>>>>>> 5eb349de90c5b940f83fa739abe146224c43a375
